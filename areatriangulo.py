85253c57bbd63ec983a30b6ccdf2c12a95bca54d
EdwardAbarca <>
2021-05-05 17:25:25 +0000

    Ejercicio 01

---
+++ areatriangulo.py
@@ -0,0 +1,9 @@
+#Primero definir las variables y otros
+print("Ejercicio 01: Área de un triangulo")
+#Datos de entrada mediante el teclado
+b=int(input("Ingrese la base: "))
+a=int(input("Ingrese la altura: "))
+#Proceso
+area=(b*a)/2
+#Datos de salida de dispositivos de salida
+print("Area de triangulo:", area)
